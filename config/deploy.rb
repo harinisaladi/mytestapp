a6d9148ff6bca95f40be701c49b20cfba6f72472
Harini Saladi <harini@gurupanguji.com>
2013-11-22 15:39:15 -0800

    updates to make errors go away again and again

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,8 @@ set :repository, "git@github.com:harinisaladi/mytestapp.git"
 set :scm_passphrase, ""
  
 set :user, "ec2-user"
-set :user_sudo, false
 set :deploy_to, "/var/www/apps/mytestapp"
+set :deploy_via, :remote_cache
  
 set :location, "ec2-54-193-11-158.us-west-1.compute.amazonaws.com"
 
