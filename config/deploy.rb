4912da20a89ae06955277a68ee07515f0c626db6
Harini Saladi <harini@gurupanguji.com>
2013-11-22 15:38:10 -0800

    updates to make errors go away again

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,14 @@ set :repository, "git@github.com:harinisaladi/mytestapp.git"
 set :scm_passphrase, ""
  
 set :user, "ec2-user"
+set :user_sudo, false
 set :deploy_to, "/var/www/apps/mytestapp"
  
-server "ec2-54-193-11-158.us-west-1.compute.amazonaws.com", :app, :primary => true
+set :location, "ec2-54-193-11-158.us-west-1.compute.amazonaws.com"
+
+role :web, location
+role :app, location
+role :db, location, :primary => true
+
+set :ssh_options, { :forward_agent => true }
+ssh_options[:keys] = %w(~/.ec2/mytest.pem ~/.ssh/github_rsa.pub)
